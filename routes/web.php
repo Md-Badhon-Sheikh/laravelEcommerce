0a2a3e26a87264c4a76b97d7ce9d62adf677d8f2
Md-Badhon-Sheikh <badhon0854@gmail.com>
2026-04-03 01:20:08 +0600

    connect login and register page

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,21 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\User\UserController;
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Middleware\AuthAdmin;
 
 
 Auth::routes();
 
 Route::get('/', [HomeController::class, 'index'])->name('home.index');
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/account-dashboard', [UserController::class, 'index'])->name('user.index');
+});
+
+
+Route::middleware(['auth', 'AdminMiddleware'])->group(function () {
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+});
