188cf21de1432913c7c10423be9ba5c7108358fe
Md-Badhon-Sheikh <badhon0854@gmail.com>
2026-04-04 21:54:08 +0600

    add logout for admin and user

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,17 +1,25 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">User Dashboard</div>
-
-                <div class="card-body">
-                    You are logged in as a User!
-                </div>
-            </div>
+  <main class="pt-90">
+    <div class="mb-4 pb-4"></div>
+    <section class="my-account container">
+      <h2 class="page-title">My Account</h2>
+      <div class="row">
+        <div class="col-lg-3">
+          @include('user.account-nav')
         </div>
-    </div>
-</div>
+        <div class="col-lg-9">
+          <div class="page-content my-account__dashboard">
+            <p>Hello <strong>User</strong></p>
+            <p>From your account dashboard you can view your <a class="unerline-link" href="account_orders.html">recent
+                orders</a>, manage your <a class="unerline-link" href="account_edit_address.html">shipping
+                addresses</a>, and <a class="unerline-link" href="account_edit.html">edit your password and account
+                details.</a></p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+
 @endsection
